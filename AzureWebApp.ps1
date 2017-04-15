9d39f173864c740640932e859f10470f31eb1b16
bhavenjp <bhavenjp@gmail.com>
2017-04-15 17:24:51 +0800

    AzureWebApp.ps1

---
+++ AzureWebApp.ps1
@@ -0,0 +1,27 @@
+Login-AzureRmAccount
+Set-AzureRmContext -SubscriptionName "Free Trial"
+
+
+$Location = "Southeast Asia"
+$RGName = "myWebAppRG"
+$Tier = "Free"
+$AppsServicePlan = "myWebAppService"
+$AppName = "bjpapp"
+
+$PropertiesObject = @{
+    repoUrl = "https://github.com/bhavenjp/myFirstSite";
+    branch = "master";
+}
+
+
+# Create Restoure Group
+New-AzureRmResourceGroup -Name $RGName -Location $Location
+
+# Create App Service Plan
+New-AzureRmAppServicePlan -Name $AppsServicePlan -Location $Location -ResourceGroupName $RGName -Tier $Tier
+
+# Create Web App
+New-AzureRmWebApp -Name $AppName -ResourceGroupName $RGName -Location $Location -AppServicePlan $AppServicePlan 
+
+# Modify Web App property to sync with GitHub
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $RGName -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $AppName/web -ApiVersion 2015-08-01 -Force
